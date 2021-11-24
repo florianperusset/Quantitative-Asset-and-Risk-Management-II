c284f296e8ceb7b43abb1bd10421f72a9c4adba0
Florian Vogt <92813754+VOGTflorian@users.noreply.github.com>
2021-11-24 15:47:29 +0100

    Add files via upload

--- factor_building.py
+++ factor_building.py
@@ -7,19 +7,38 @@ Created on Wed Nov 24 15:08:53 2021
 """
 import numpy as np
 
-def factor_building(returns, quantile=0.5, long_above_quantile=True):
-    quantile_factor = returns.quantile(q=quantile, axis=1)
-    position_factor = returns.copy()
+def factor_building(metric, quantile=0.5, long_above_quantile=True):
+    """
+    Builds a factor, that is, determines the weights in each security within the factor
+    
+    Parameters
+    ----------
+    metric : DataFrame
+        the metric used to build the factor.
+    quantile : int, optional
+        quantile to build the factor. The default is 0.5.
+    long_above_quantile : boolean, optional
+        determines if we long the securities above or below the quantile in the construction of the factor. The default is True.
+
+    Returns
+    -------
+    position_factor : DataFrame
+        the DataFrame with the weights in each security in the factor.
+
+    """
+    quantile_factor = metric.quantile(q=quantile, axis=1)
+    position_factor = metric.copy()
     
     if long_above_quantile:
         for i in position_factor.columns:
-            position_factor.loc[returns[i] >= quantile_factor, i] = 1
-            position_factor.loc[returns[i] < quantile, i] = 0
+            position_factor.loc[metric[i] >= quantile_factor, i] = 1
+            position_factor.loc[metric[i] < quantile_factor, i] = 0
     else:
         for i in position_factor.columns:
-            position_factor.loc[returns[i] >= quantile_factor, i] = 0
-            position_factor.loc[returns[i] < quantile, i] = 1
-    return position_factor.div(position_factor.sum(axis=1), axis=0).replace(np.nan,0)
+            position_factor.loc[metric[i] >= quantile_factor, i] = 0
+            position_factor.loc[metric[i] < quantile_factor, i] = 1
+    position_factor = position_factor.div(position_factor.sum(axis=1), axis=0).replace(np.nan,0)
+    return position_factor
 
 
 
